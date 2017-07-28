e63cbf362444d2611eef07382b02ded06eb63eb3
Marcos Vinicius Monteiro Araujo <marcosaraujo.mvma@gmail.com>
2017-07-27 22:13:57 -0300

    chaves

--- simulacao/keys/gerado_de_chaves_RSA.py
+++ simulacao/keys/gerado_de_chaves_RSA.py
@@ -6,16 +6,18 @@ rng = Random.new().read
 RSAkey = RSA.generate(1024, rng)
 
 """
-pool = RandomPool(1024)
+bits =1024
+
+pool = RandomPool(bits)
 pool.stir()
 randfunc = pool.get_bytes
-RSAkey= RSA.generate(1024, randfunc)
+RSAkey= RSA.generate(bits, randfunc)
 
-f = open("meter-private.pem","w+")
+f = open("inmetro-private.pem","w+")
 f.write(RSAkey.exportKey("PEM"))
 f.close()
 print "Gerou chave Privada"
-f = open("meter-public","w+")
+f = open("inmetro-public","w+")
 f.write(RSAkey.publickey().exportKey("PEM"))
 f.close()
 print "Gerou chave Publica"
